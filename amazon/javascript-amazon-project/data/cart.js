5cd4a97d17bcb298ddd18f14e0e069d1f8dac106
Kovacs_Roland_Gergo <kovaxrawland@mailbox.unideb.hu>
2024-07-24 15:11:50 +0300

    feat: created localStorage and now the cart is updated after each added item

--- amazon/javascript-amazon-project/data/cart.js
+++ amazon/javascript-amazon-project/data/cart.js
@@ -1,13 +1,11 @@
-export let cart = [
-    {
-        productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-        quantity:2
-    },
-    {
-        productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
-        quantity:1
-    }
+export let cart =JSON.parse(localStorage.getItem('cart')) || [
+    { productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6', quantity: 2 },
+    { productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d', quantity: 1 }
 ];
+
+function  saveToStorage(){
+    localStorage.setItem('cart',JSON.stringify(cart));
+}
 export let cartQuantity = 0;
 export const addedMessageTimeouts = {};
 
@@ -31,6 +29,7 @@ export function addToCart(button) {
     document.querySelector('.js-cart-quantity').textContent = cartQuantity;
 
     updateCartQuantity(productId);
+    saveToStorage();
 }
 
 export function updateCartQuantity(productId) {
@@ -59,4 +58,5 @@ export function removeFromCart(productId){
         }
     });
     cart=newCart;
+    saveToStorage();
 }
